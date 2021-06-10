cf67cf89e535b68f06f16d17a745ce6e27db12a7
asharabuhelwh <ashar.mohamad@yahoo.com>
2021-06-11 02:13:07 +0300

    add the user stories and done with the basic auth

--- src/auth/routes.js
+++ src/auth/routes.js
@@ -1,2 +1,34 @@
 'use strict';
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const User = require ('./models/users-model.js');
+const basicAuth = require('./middleware/basic.js');
+
+router.post('/signup', async (req, res, next) => {
+  try {
+    let user = new User(req.body);
+    const userRecord = await user.save();
+    const output = {
+      user: userRecord,
+      token: userRecord.token
+    };
+    res.status(201).json(output);
+  } catch (e) {
+    next(e.message)
+  }
+});
+
+router.post('/signin', basicAuth, (req, res, next) => {
+  const user = {
+    user: req.user,
+    token: req.user.token
+  };
+  res.status(200).json(user);
+});
+
+
+
+
+module.exports = router;
 
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const router =require('./auth/routes.js')
 
 // Esoteric Resources
 const errorHandler = require('./error-handlers/500.js');
@@ -21,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use();
+app.use('/',router);
 
 // Catchalls
 app.use(notFound);
