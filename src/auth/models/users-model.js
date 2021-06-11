96ca08eab59f0256b9abc81ce351da17e49a43e2
jdeitawimostafa <jdeitawimostafa@gmail.com>
2021-06-11 22:56:03 +0300

    merging between the models files

--- src/auth/models/users-model.js
+++ src/auth/models/users-model.js
@@ -7,7 +7,16 @@ const bcrypt = require('bcrypt');
 const users = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  role:{type:String,required:true,default:'user',enum:['user','admin']},
   //add role here 
+});
+
+users.virtual('capabilities').get(function(){
+  let acl = {
+    user:['read','create','update','delete'],
+    admin:['read','create','update','delete'],
+  };
+  return acl[this.role];
 });
 
 //****Basic Auth*****//
