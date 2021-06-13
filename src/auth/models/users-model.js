9cbd045be743c01a0be3038f965116076ff61513
engnour94 <nour.abuelenein@hotmail.com>
2021-06-13 20:12:09 +0300

    done

--- src/auth/models/users-model.js
+++ src/auth/models/users-model.js
@@ -5,9 +5,18 @@ const bcrypt = require('bcrypt');
 
 //user schema 
 
+const messagesSchema = new mongoose.Schema({
+  message:{type:String},
+});
 
-<<<<<<< HEAD
-=======
+const dashboardSchema = mongoose.Schema({
+  descriptionOfService:{type:String},
+});
+
+const notificationsSchema = mongoose.Schema({
+  link:{type:String},
+});
+
 const users = new mongoose.Schema({
   // email:{type:String,required:true,unique:true},
   name:{type:String},
@@ -24,33 +33,6 @@ const users = new mongoose.Schema({
 
 });
 
->>>>>>> 439990c911012c6239e7742e239e69a8aad54c75
-const messagesSchema = new mongoose.Schema({
-  message:{type:String},
-});
-
-const dashboardSchema = mongoose.Schema({
-  descriptionOfService:{type:String},
-});
-
-const notificationsSchema = mongoose.Schema({
-  link:{type:String},
-});
-const users = new mongoose.Schema({
-  // email:{type:String,required:true,unique:true},
-  // name:{type:String,required:true},
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  // service:{type:String,required:true},
-  // experience:{type:String,required:true},
-  // descriptionOfUser:{type:String,required:true},
-  // messages:[messagesSchema],
-  // dashboard:[dashboardSchema],
-  // notifications:[notificationsSchema],
-  // role:{type:String,required:true,default:'user',enum:['user','admin']},
-  //add role here 
-
-});
 
 users.virtual('capabilities').get(function(){
   let acl = {
@@ -99,5 +81,3 @@ users.statics.authenticateWithToken = async function (token) {
 
 
 module.exports = mongoose.model('users', users);
-
-
