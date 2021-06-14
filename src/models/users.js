e6f43ed94c03374682a6590bf62fb4b2f1a4dcbd
jdeitawimostafa <jdeitawimostafa@gmail.com>
2021-06-15 00:18:02 +0300

    editing some issues

--- src/models/users.js
+++ src/models/users.js
@@ -4,6 +4,10 @@ const users = [];
 
 // Join user to chat
 function userJoin(id, username, room) {
+  if(!id || !username || !room){
+    throw new Error('invalid data !!');
+  }
+
   const user = { id, username, room };
 
   users.push(user);
@@ -13,11 +17,19 @@ function userJoin(id, username, room) {
 
 // Get current user
 function getCurrentUser(id) {
+  if(!id){
+    throw new Error('invalid data !!');
+  }
   return users.find(user => user.id === id);
 }
 
 // User leaves chat
 function userLeave(id) {
+
+  if(!id){
+    throw new Error('invalid data !!');
+  }
+
   const index = users.findIndex(user => user.id === id);
 
   if (index !== -1) {
@@ -27,6 +39,11 @@ function userLeave(id) {
 
 // Get room users
 function getRoomUsers(room) {
+
+  if(!room){
+    throw new Error('invalid data !!');
+  }
+  
   return users.filter(user => user.room === room);
 }
 
